6f99e0512c1503831639cb26292dd0d37a496818
Hoang Thuy Vo Anh <hoangtvasr150780@gmail.com>
2021-09-24 12:22:23 +0700

    Change MouseEnter function

--- src/components/_external-pages/customer-app/CustomerFeatures.tsx
+++ src/components/_external-pages/customer-app/CustomerFeatures.tsx
@@ -33,7 +33,7 @@ const HeroImgStyle = styled(motion.img)(({ theme }) => {
     borderRadius: '8px',
     boxShadow: `0px 100px 80px 0 ${shadowCard(0.4)}`,
     height: '',
-    width: 300,
+    width: 300
   };
 });
 
@@ -46,7 +46,7 @@ const CardShadowLeft = styled(Card)(({ theme }) => {
   return {
     margin: 'auto',
     padding: theme.spacing(4, 2, 4),
-    boxShadow: `-20px 20px 20px 10px ${shadowCard(0.2)}`,
+    boxShadow: `-20px 20px 20px 10px ${shadowCard(0.2)}`
   };
 });
 
@@ -59,7 +59,7 @@ const CardShadowRight = styled(Card)(({ theme }) => {
   return {
     margin: 'auto',
     padding: theme.spacing(4, 2, 4),
-    boxShadow: `20px 20px 20px 10px ${shadowCard(0.2)}`,
+    boxShadow: `20px 20px 20px 10px ${shadowCard(0.2)}`
   };
 });
 
@@ -67,181 +67,114 @@ export default function EnterpriseFeatures() {
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
 
-  let srcNotHover = "/static/customer-app/1.webp";
-  let src1 = "/static/customer-app/1.webp";
-  let src2 = "/static/customer-app/2.webp";
-  let src3 = "/static/customer-app/3.webp";
-  let src4 = "/static/customer-app/4.webp";
-  let src5 = "/static/customer-app/5.webp";
-  let src6 = "/static/customer-app/6.webp";
-  let src7 = "/static/customer-app/7.webp";
-  let src8 = "/static/customer-app/8.webp";
-  
-  let [src, setSrc] = useState(
-    src1 && src2 && src3 && src4 && src5 && src6 && src7 && src8 );
-  
-  const mouseEnter1 = () => {
-    setSrc(src1);
-  }
-  const mouseLeave1 = () => {
-    setSrc(srcNotHover);
-  }
+  const [id, setId] = useState(1);
 
-  
-  const mouseEnter2 = () => {
-    setSrc(src2);
-  }
-  const mouseLeave2 = () => {
-    setSrc(srcNotHover);
-  }
+  const list = [
+    {
+      id: '1',
+      title: 'Hoàn toàn có thể tùy chỉnh',
+      txt: 'Thiết kế quy trình theo Trường hợp sử dụng kinh doanh của bạn.',
+      img: '/static/customer-app/1.webp'
+    },
 
-  
-  const mouseEnter3 = () => {
-    setSrc(src3);
-  }
-  
-  const mouseLeave3 = () => {
-    setSrc(srcNotHover);
-  }
-  
-  const mouseEnter4 = () => {
-    setSrc(src4);
-  }
-  
-  const mouseLeave4 = () => {
-    setSrc(srcNotHover);
-  }
-  
-  const mouseEnter5 = () => {
-    setSrc(src5);
-  }
-  
-  const mouseLeave5 = () => {
-    setSrc(srcNotHover);
-  }
-  
-  const mouseEnter6 = () => {
-    setSrc(src6);
-  }
-  
-  const mouseLeave6 = () => {
-    setSrc(srcNotHover);
-  }
-  
-  const mouseEnter7 = () => {
-    setSrc(src7);
-  }
-  
-  const mouseLeave7 = () => {
-    setSrc(srcNotHover);
-  }
-  
-  const mouseEnter8 = () => {
-    setSrc(src8);
-  }
-  const mouseLeave8 = () => {
-    setSrc(srcNotHover);
-  }
+    {
+      id: '2',
+      title: 'Định giá dựa trên hàng rào địa lý',
+      txt: 'Sử dụng định giá dựa trên thẻ và khu vực cho nhu cầu bản địa hóa.',
+      img: '/static/customer-app/2.webp'
+    },
+
+    {
+      id: '3',
+      title: 'Sự liên lạc khẩn cấp',
+      txt: 'Khách hàng có thể gọi điện hoặc trò chuyện với đại lý bằng ứng dụng chỉ với một cú nhấp chuột',
+      img: '/static/customer-app/3.webp'
+    },
+
+    {
+      id: '4',
+      title: 'Hiệu suất và Xếp hạng',
+      txt: 'Nhận phản hồi và xếp hạng dễ dàng từ khách hàng để lập kế hoạch hành động trong tương lai.',
+      img: '/static/customer-app/4.webp'
+    },
+    
+    {
+      id: '5',
+      title: 'Thông báo và Cảnh báo',
+      txt: 'Cập nhật trạng thái đơn hàng trực tiếp cho khách hàng để nâng cao trải nghiệm.',
+      img: '/static/customer-app/5.webp'
+    },
+
+    {
+      id: '6',
+      title: 'Theo dõi thời gian thực',
+      txt: 'Theo dõi vị trí chính xác trên bản đồ và giao hàng ETA.',
+      img: '/static/customer-app/6.webp'
+    },
+
+    {
+      id: '7',
+      title: 'Ví khách hàng tích hợp sẵn',
+      txt: 'Cung cấp cho khách hàng sự linh hoạt khi nạp tiền một lần.',
+      img: '/static/customer-app/7.webp'
+    },
+
+    {
+      id: '8',
+      title: 'Khuyến mãi và ưu đãi',
+      txt: 'Đẩy giảm giá và khuyến mại cho khách hàng bằng cách sử dụng mã phiếu giảm giá và thông báo.',
+      img: '/static/customer-app/8.webp'
+    }
+  ];
 
   return (
     <>
       <RootStyle initial="initial" animate="animate" variants={varWrapEnter}>
         <Container maxWidth="lg">
           <Stack spacing={[8, 12]}>
-            <MotionInView variants={varFadeInUp} sx={{ mb: { xs: 10, md: 15 }, textAlign: 'center' }}>
-                <Typography variant="h3">
-                Ứng dụng khách hàng TZM sẽ giúp ích gì cho doanh nghiệp của bạn?              
-                </Typography>
+            <MotionInView
+              variants={varFadeInUp}
+              sx={{ mb: { xs: 10, md: 15 }, textAlign: 'center' }}
+            >
+              <Typography variant="h3">
+                Ứng dụng khách hàng TZM sẽ giúp ích gì cho doanh nghiệp của bạn?
+              </Typography>
             </MotionInView>
             <MotionInView variants={varFadeInLeft}>
               <Stack direction={{ xs: 'column', md: 'row' }} spacing={12}>
                 <Stack spacing={6} sx={{ flex: 1 }}>
-                  <CardShadowLeft>  
-                    <Typography variant="h6"  onMouseEnter={mouseEnter1} onMouseLeave={mouseLeave1}>
-                      Hoàn toàn có thể tùy chỉnh
-                      <Typography variant="body1">
-                      Thiết kế quy trình theo Trường hợp sử dụng kinh doanh của bạn.                    
-                      </Typography>              
-                    </Typography>
-                  </CardShadowLeft>
-                  <CardShadowLeft>  
-                  <Typography variant="h6" onMouseEnter={mouseEnter2} onMouseLeave={mouseLeave2}>
-                    Định giá dựa trên hàng rào địa lý
-                    <Typography variant="body1">
-                    
-                    Sử dụng định giá dựa trên thẻ và khu vực cho nhu cầu bản địa hóa
-                    </Typography>              
-                  </Typography>
-                  </CardShadowLeft>  
-                  <CardShadowLeft>  
-                  <Typography variant="h6" onMouseEnter={mouseEnter3} onMouseLeave={mouseLeave3}>
-                    Sự liên lạc khẩn cấp
-                    <Typography variant="body1">
-                    
-                    Khách hàng có thể gọi điện hoặc trò chuyện với đại lý 
-                    bằng ứng dụng chỉ với một cú nhấp chuột                    
-                    </Typography>              
-                  </Typography>
-                  </CardShadowLeft>  
-                  <CardShadowLeft>  
-                  <Typography variant="h6" onMouseEnter={mouseEnter4} onMouseLeave={mouseLeave4}>
-                    Hiệu suất và Xếp hạng
-                    <Typography variant="body1">
-                    
-                    Nhận phản hồi và xếp hạng dễ dàng từ khách hàng 
-                    để lập kế hoạch hành động trong tương lai
-                    </Typography>              
-                  </Typography>
-                  </CardShadowLeft>  
+                  {list.slice(0, 4).map((item) => (
+                    <div onMouseOver={() => setId(parseInt(item.id))}>
+                      <CardShadowLeft>
+                        <Typography variant="h6">
+                          {item.title}
+                          <Typography variant="body1">{item.txt}</Typography>
+                        </Typography>
+                      </CardShadowLeft>
+                    </div>
+                  ))}
                 </Stack>
                 <Box>
                   <HeroImgStyle
-                  sx={{ 
-                    position: 'relative',
-                    top: 70
-                    
-                  }} 
-                  alt="feature-1" 
-                  src={src}
+                    sx={{
+                      position: 'relative',
+                      top: 70
+                    }}
+                    alt="feature"
+                    src={list[id - 1].img}  
                   />
                 </Box>
                 <Stack spacing={6} sx={{ flex: 1 }}>
-                  <CardShadowRight> 
-                    <Typography variant="h6" onMouseEnter={mouseEnter5} onMouseLeave={mouseLeave5}>
-                      Thông báo và Cảnh báo
-                      <Typography variant="body1" >         
-                      Cập nhật trạng thái đơn hàng trực tiếp cho khách hàng để nâng cao trải nghiệm
-                      </Typography>              
-                    </Typography>
-                  </CardShadowRight> 
-                  
-                  <CardShadowRight> 
-                    <Typography variant="h6" onMouseEnter={mouseEnter6} onMouseLeave={mouseLeave6}>
-                      Theo dõi thời gian thực
-                      <Typography variant="body1">
-                      Theo dõi vị trí chính xác trên bản đồ và giao hàng ETA
-
-                      </Typography>              
-                    </Typography>
-                  </CardShadowRight> 
-                  
-                  <CardShadowRight> 
-                    <Typography variant="h6" onMouseEnter={mouseEnter7} onMouseLeave={mouseLeave7}>
-                      Ví khách hàng tích hợp sẵn
-                      <Typography variant="body1">
-                      Cung cấp cho khách hàng sự linh hoạt khi nạp tiền một lần
-                      </Typography>              
-                    </Typography>
-                  </CardShadowRight> 
-
-                  <CardShadowRight> 
-                    <Typography variant="h6" onMouseEnter={mouseEnter8} onMouseLeave={mouseLeave8}>
-                      Khuyến mãi và ưu đãi
-                      <Typography variant="body1">
-                      Đẩy giảm giá và khuyến mại cho khách hàng bằng cách sử dụng mã phiếu giảm giá và thông báo
-                      </Typography>              
-                    </Typography>
-                  </CardShadowRight> 
+                  {list.slice(4).map((item) => (
+                    <div onMouseOver={() => setId(parseInt(item.id))}>
+                      <CardShadowRight>
+                        <Typography variant="h6">
+                          {item.title}
+                          <Typography variant="body1">{item.txt}</Typography>
+                        </Typography>
+                      </CardShadowRight>
+                    </div>
+                  ))}
                 </Stack>
               </Stack>
             </MotionInView>
--- src/components/_external-pages/enterprise/EnterpriseFeatures.tsx
+++ src/components/_external-pages/enterprise/EnterpriseFeatures.tsx
@@ -1,7 +1,7 @@
 import { alpha, Container, Stack, Typography, useMediaQuery, Box, Button } from '@material-ui/core';
 import { styled, useTheme } from '@material-ui/core/styles';
 import { motion } from 'framer-motion';
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 //
 import { MotionInView, varFadeInLeft, varFadeInRight, varWrapEnter } from '../../animate';
 
@@ -44,76 +44,73 @@ const HeroImgStyle = styled(motion.img)(({ theme }) => {
   };
 });
 
-
 export default function EnterpriseFeatures() {
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
 
+  const [id, setId] = useState(1);
 
   //hover features of "Tạo tác vụ"
-  let srcNotHover = "/static/enterprise/pos.jpg";
-  let srcHoverForm = "/static/enterprise/form.jpg";
-  let srcHoverApp = "/static/enterprise/app.jpg";
-  let srcHoverManage = "/static/enterprise/manage.jpg";
-  
-  let [src, setSrc] = useState(
-    srcHoverManage && srcHoverForm && srcHoverApp );
-  
-  const mouseEnterForm = () => {
-    setSrc(srcHoverForm);
-  }
-  const mouseLeaveForm = () => {
-    setSrc(srcNotHover);
-  }
+  const list = [
+    {
+      id: '1',
+      title: '1. Bảng điều khiển',
+      txt: 'Tạo một hoặc nhiều tác vụ trong một lần thông qua bảng điều khiển.',
+      img: '/static/enterprise/pos.jpg'
+    },
 
-  
-  const mouseEnterApp = () => {
-    setSrc(srcHoverApp);
-  }
-  const mouseLeaveApp = () => {
-    setSrc(srcNotHover);
-  }
+    {
+      id: '2',
+      title: '2. Biểu mẫu',
+      txt: 'Biểu mẫu cho phép bạn tạo các nhiệm vụ ngay lập tức trong bảng điều khiển bằng cách chấp nhận đơn đặt hàng từ khách hàng của bạn bằng cách sử dụng biểu mẫu đặt phòng trên web.',
+      img: '/static/enterprise/form.jpg'
+    },
 
-  
-  const mouseEnterManage = () => {
-    setSrc(srcHoverManage);
-  }
-  const mouseLeaveManage = () => {
-    setSrc(srcNotHover);
-  }
+    {
+      id: '3',
+      title: '3. Các ứng dụng của khách hàng',
+      txt: 'Nhận đơn đặt hàng trực tiếp của khách hàng thông qua các ứng dụng có sẵn trên cả Android và iOS.',
+      img: '/static/enterprise/app.jpg'
+    },
+
+    {
+      id: '4',
+      title: '4. Nền tảng quản lý đơn hàng / Marketplaces / POS / API',
+      txt: 'Tạo đơn đặt hàng của bạn thông qua trang web của bên thứ 3 một cách suôn sẻ và hiệu quả.',
+      img: '/static/enterprise/manage.jpg'
+    }
+  ];
 
   //hover features of "Phân tích & báo cáo"
-  let srcOrigin = "/static/enterprise/custom.jpg";
-  let srcHover1 = "/static/enterprise/time.jpg";
-  let srcHover2 = "/static/enterprise/rate.jpg";
-  let srcHover3 = "/static/enterprise/finance.jpg";
-  
-  let [source, setSource] = useState(
-    srcHover1 && srcHover2 && srcHover3 );
-  
-  const mouseEnter1 = () => {
-    setSource(srcHover1);
-  }
-  const mouseLeave1 = () => {
-    setSource(srcOrigin);
-  }
+  const list2 = [
+    {
+      id: '1',
+      title: '1. Hiệu quả dựa trên thời gian và khoảng cách',
+      txt: 'Nhận ngay báo cáo hiệu quả của các đại lý của bạn trên bảng điều khiển dựa trên Thời gian và Khoảng cách.',
+      img: '/static/enterprise/custom.jpg'
+    },
 
-  
-  const mouseEnter2 = () => {
-    setSource(srcHover2);
-  }
-  const mouseLeave2 = () => {
-    setSource(srcOrigin);
-  }
+    {
+      id: '2',
+      title: '2. Phân tích thời gian thực',
+      txt: 'Tăng hiệu quả của đại lý và sự hài lòng của khách hàng bằng cách sử dụng báo cáo thời gian thực của mọi tác vụ.',
+      img: '/static/enterprise/time.jpg'
+    },
 
-  
-  const mouseEnter3 = () => {
-    setSource(srcHover3);
-  }
-  const mouseLeave3 = () => {
-    setSource(srcOrigin);
-  }
- 
+    {
+      id: '3',
+      title: '3. Theo dõi và đánh giá người giao hàng',
+      txt: 'Giám sát quá trình làm việc, vận chuyển dựa trên đánh giá thực tế của khách hàng và minh bạch hơn với khách hàng cuối.',
+      img: '/static/enterprise/rate.jpg'
+    },
+
+    {
+      id: '4',
+      title: '4. Định giá và thu nhập',
+      txt: 'Quản lý các khoản thanh toán cho tài xế một cách hiệu quả với Ví đại lý.',
+      img: '/static/enterprise/finance.jpg'
+    }
+  ];
 
   return (
     <>
@@ -124,53 +121,32 @@ export default function EnterpriseFeatures() {
               <Stack direction={{ xs: 'column-reverse', md: 'row' }} spacing={4}>
                 <Stack spacing={6} sx={{ flex: 1 }}>
                   <Typography variant="h3">Tạo tác vụ</Typography>
-                  <Typography variant="h6">
-                    1. Bảng điều khiển - một hay nhiều tác vụ hay CSV 
-                    <Typography variant="body1">
-                    Tạo một hoặc nhiều tác vụ trong một lần thông qua bảng điều khiển TZM.
-                    </Typography>              
-                  </Typography>
-                  
-                  <Typography variant="h6" onMouseEnter={mouseEnterForm} onMouseLeave={mouseLeaveForm}>
-                    2. Biểu mẫu
-                    <Typography variant="body1">
-                    Biểu mẫu TZM cho phép bạn tạo các tác vụ ngay lập tức trong bảng điều khiển 
-                    bằng cách chấp nhận đơn đặt hàng từ khách hàng của bạn qua việc sử dụng biểu mẫu đặt phòng trên web.
-                    </Typography>              
-                  </Typography>
-
-                  <Typography variant="h6" onMouseEnter={mouseEnterApp} onMouseLeave={mouseLeaveApp}>
-                    3. Ứng dụng của khách hàng
-                    <Typography variant="body1">
-                    Nhận đơn đặt hàng trực tiếp của khách hàng thông qua các ứng dụng có sẵn.
-                    </Typography>              
-                  </Typography>
-
-                  <Typography variant="h6" onMouseEnter={mouseEnterManage} onMouseLeave={mouseLeaveManage}>
-                    4. Nền tảng quản lý đơn hàng / Thị trường / POS / API
-                    <Typography variant="body1">
-                    Tạo đơn đặt hàng của bạn thông qua trang web của bên thứ 3 một cách suôn sẻ và hiệu quả.
-                    </Typography>              
-                  </Typography>
+                  {list.map((item) => (
+                    <div onMouseOver={() => setId(parseInt(item.id))}>
+                      <Typography variant="h6">
+                        {item.title}
+                        <Typography variant="body1">{item.txt}</Typography>
+                      </Typography>
+                    </div>
+                  ))}
 
                   <Button
-                  size="large"
-                  variant="contained"
-                  disableElevation
-                  sx={{ width: '125px', height: ['48px', '100%'] }}
+                    size="large"
+                    variant="contained"
+                    disableElevation
+                    sx={{ width: '125px', height: ['48px', '100%'] }}
                   >
-                  Bắt đầu
+                    Bắt đầu
                   </Button>
                 </Stack>
                 <Box>
                   <HeroImgStyle
-                  sx={{ 
-                    position: 'relative',
-                    top: 90
-                    
-                  }} 
-                  alt="feature-1" 
-                  src={src}
+                    sx={{
+                      position: 'relative',
+                      top: 90
+                    }}
+                    alt="feature-1"
+                    src={list[id - 1].img}
                   />
                 </Box>
               </Stack>
@@ -178,59 +154,36 @@ export default function EnterpriseFeatures() {
             <MotionInView variants={varFadeInRight}>
               <Stack direction={{ xs: 'column-reverse', md: 'row-reverse' }} spacing={4}>
                 <Stack spacing={6} sx={{ flex: 1 }}>
-                  <Typography variant="h3">
-                  Phân tích & Báo cáo
-                  </Typography>
-                  
-                  <Typography variant="h6">
-                    1. Hiệu quả dựa trên thời gian và khoảng cách
-                    <Typography variant="body1">
-                    Nhận ngay báo cáo hiệu quả của các đại lý của bạn trên bảng điều khiển 
-                    dựa trên Thời gian và Khoảng cách.
-                    </Typography>              
-                  </Typography>
-                  
-                  <Typography variant="h6" onMouseEnter={mouseEnter1} onMouseLeave={mouseLeave1}>
-                    2. Phân tích thời gian thực
-                    <Typography variant="body1">
-                    Tăng hiệu quả của đại lý và sự hài lòng của khách hàng 
-                    bằng cách sử dụng báo cáo thời gian thực của mọi tác vụ.
-                    </Typography>              
-                  </Typography>
+                  <Typography variant="h3">Phân tích & Báo cáo</Typography>
 
-                  <Typography variant="h6" onMouseEnter={mouseEnter2} onMouseLeave={mouseLeave2}>
-                    3. Theo dõi và đánh giá người giao hàng
-                    <Typography variant="body1">
-                    Giám sát quá trình làm việc, vận chuyển dựa trên đánh giá thực tế của khách hàng 
-                    và minh bạch hơn với khách hàng cuối.
-                    </Typography>              
-                  </Typography>
-
-                  <Typography variant="h6"  onMouseEnter={mouseEnter3} onMouseLeave={mouseLeave3}>
-                    4. Định giá và thu nhập
-                    <Typography variant="body1">
-                    Quản lý các khoản thanh toán cho tài xế một cách hiệu quả với Ví đại lý.
-                    </Typography>              
-                  </Typography>
+                  {list2.map((item) => (
+                    <div onMouseOver={() => setId(parseInt(item.id))}>
+                      <Typography variant="h6">
+                        {item.title}
+                        <Typography variant="body1">{item.txt}</Typography>
+                      </Typography>
+                    </div>
+                  ))}
 
                   <Button
-                  size="large"
-                  variant="contained"
-                  disableElevation
-                  sx={{ width: '125px', height: ['48px', '100%'] }}
+                    size="large"
+                    variant="contained"
+                    disableElevation
+                    sx={{ width: '125px', height: ['48px', '100%'] }}
                   >
-                  Bắt đầu
+                    Bắt đầu
                   </Button>
                 </Stack>
+               
                 <Box>
-                
-                  <HeroImgStyle 
-                  sx={{ 
-                    position: 'relative',
-                    top: 100
-                    
-                  }} 
-                  alt="feature-1" src={source} />
+                  <HeroImgStyle
+                    sx={{
+                      position: 'relative',
+                      top: 100
+                    }}
+                    alt="feature-2"
+                    src={list2[id - 1].img}
+                  />
                 </Box>
               </Stack>
             </MotionInView>
